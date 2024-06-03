dc46795856c66eb5f9079c9b0bf6afb52086cab2
ayan878 <ayanraza197@gmail.com>
2024-06-04 00:03:17 +0530

    13.RomanToInteger

--- Easy/RomanToInteger/Solution.java
+++ Easy/RomanToInteger/Solution.java
@@ -1,17 +1,33 @@
 package Easy.RomanToInteger;
+
 import java.util.HashMap;
-import java.util.Map;
+
 
 public class Solution {
-    int value=0;
-    Map<Character,Integer> map=new HashMap<>();
-    public static int RomanToInteger(String[] args) {
-    for(int i=0;i<map.size();i++){
-        if(map)
-    }
+
+    public static int romanToInteger(String s) {
+
+        HashMap<Character, Integer> map = new HashMap<>();    
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int value = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                value -= map.get(s.charAt(i));
+            } else {
+                value += map.get(s.charAt(i));
+            }
+        }
         return value;
     }
+
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println(romanToInteger("II")); // Output: 2
     }
 }
