5c7e05dd174bdcb2c54ea0089a090f12fdfa8502
ayan878 <ayanraza197@gmail.com>
2024-06-03 23:28:19 +0530

    13.RomanToInteger

---
+++ Easy/RomanToInteger/Solution.java
@@ -0,0 +1,17 @@
+package Easy.RomanToInteger;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    int value=0;
+    Map<Character,Integer> map=new HashMap<>();
+    public static int RomanToInteger(String[] args) {
+    for(int i=0;i<map.size();i++){
+        if(map)
+    }
+        return value;
+    }
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
