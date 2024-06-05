5ddede0c5f0c2ff84a314e7f534f7833caee06d0
ayan878 <ayanraza197@gmail.com>
2024-06-06 01:52:30 +0530

    Remove Element

---
+++ Easy/RemoveElement/Solution.java
@@ -0,0 +1,24 @@
+package Easy.RemoveElement;
+
+public class Solution {
+    public static int removeElement(int[] nums, int val) {
+        int n = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[n] = nums[i];
+                n++;
+            }
+        }
+        return n;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 4, 3, 2};
+        int newLength = removeElement(nums, 2);
+        System.out.println("New length: " + newLength);
+        System.out.print("Modified array: ");
+        for (int i = 0; i < newLength; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
